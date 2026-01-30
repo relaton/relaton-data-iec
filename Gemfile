89ab144098f32dbde3ba15be7567f4083a3fb259
Andrei Kislichenko <android.2net@gmail.com>
2026-01-29 20:34:40 -0500

    create data v2

--- Gemfile
+++ Gemfile
@@ -4,4 +4,5 @@ source "https://rubygems.org"
 
 git_source(:github) { |repo_name| "https://github.com/#{repo_name}" }
 
-gem "relaton-iec", github: "relaton/relaton-iec", branch: "main"
+gem "relaton-iec", "~> 2.0.0.alpha.1" # , github: "relaton/relaton-iec", branch: "main"
+gem "pry-byebug"
--- crawler.rb
+++ crawler.rb
@@ -1,10 +1,10 @@
 # frozen_string_literal: true
 
-require "relaton_iec"
+require "relaton/iec/data_fetcher"
 
 mode = ARGV.shift || "latest"
 ENV["IEC_HAPI_PROJ_PUBS_KEY"] = ARGV.shift
 ENV["IEC_HAPI_PROJ_PUBS_SECRET"] = ARGV.shift
 
 FileUtils.rm Dir.glob('index*') unless mode == "latest"
-RelatonIec::DataFetcher.new("iec-harmonised-#{mode}").fetch
+Relaton::Iec::DataFetcher.fetch("iec-harmonised-#{mode}")
